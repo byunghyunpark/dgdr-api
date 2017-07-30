44a74ff65f25dac11e18003252f9637792936a62
byunghyunpark <openmind8735@gmail.com>
2017-07-31 01:10:39 +0900

    Add photo upload size validation

---
+++ django_app/common/constants.py
@@ -0,0 +1,5 @@
+# Image Upload options
+UPLOAD_PHOTO_MAX_SIZE = 2000000
+
+# Pagination
+PAGE_SIZE = 20
--- django_app/house/admin.py
+++ django_app/house/admin.py
@@ -1,7 +1,40 @@
-from django.contrib import admin
 import nested_admin
+from django import forms
+from django.contrib import admin
 
+from common.constants import UPLOAD_PHOTO_MAX_SIZE
 from house.models import House, SearchTag, PhotoGroup, HousePhoto, Category, Room
+
+
+class HouseForm(forms.ModelForm):
+    class Meta:
+        model = House
+        fields = "__all__"
+
+    def clean_main_photo(self):
+        main_photo = self.cleaned_data.get("main_photo")
+
+        if not main_photo:
+            raise forms.ValidationError("No photo!")
+        elif main_photo.size > UPLOAD_PHOTO_MAX_SIZE:
+            raise forms.ValidationError("Photo size should be less than {SIZE}byte".format(SIZE=UPLOAD_PHOTO_MAX_SIZE))
+        else:
+            return main_photo
+
+
+class HousePhotoForm(forms.ModelForm):
+    class Meta:
+        model = HousePhoto
+        fields = "__all__"
+
+    def clean_photo(self):
+        photo = self.cleaned_data.get("photo")
+        if not photo:
+            raise forms.ValidationError("No photo!")
+        elif photo.size > UPLOAD_PHOTO_MAX_SIZE:
+            raise forms.ValidationError("Photo size should be less than {SIZE}byte".format(SIZE=UPLOAD_PHOTO_MAX_SIZE))
+        else:
+            return photo
 
 
 class SearchTagAdmin(admin.ModelAdmin):
@@ -23,6 +56,7 @@ class RoomInline(nested_admin.NestedTabularInline):
 
 
 class HousePhotoInline(nested_admin.NestedTabularInline):
+    form = HousePhotoForm
     model = HousePhoto
     sortable_field_name = "position"
     extra = 0
@@ -36,6 +70,7 @@ class PhotoGroupInline(nested_admin.NestedTabularInline):
 
 
 class HouseAdmin(nested_admin.NestedModelAdmin):
+    form = HouseForm
     inlines = [RoomInline, PhotoGroupInline, ]
     list_display = (
         "id", "name", "opened_date", "capacity_count", "status"
